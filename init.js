eca85f2f2b8db5ce79f526ace740bf7b8b78d4ce
Helbak <helbak77@gmail.com>
2019-05-10 20:59:18 +0300

    second day

--- init.js
+++ init.js
@@ -1,14 +1,65 @@
-// const myArray = {
-//     name : null,
-//     arr : null,
-// checkParam : cP(arr),
-//
-// }
-function cP(arr) {
-   // if(arr.length>=0 || isArray(arr)){
-       if(arr.constructor === Array){
+// const array = [1,2,3,4,5];
+// const nameOfArray = "ara";
+const myArray = {
+    nameAr: nameOfArray,
+    arr: array,
+    funcCheckParam: checkParamAsArray(arr),
+    funcGetSize: getSize(arr),
+    funcDoPop: doPop(arr),
+    funcDoPush: doPush(arr, newElement),
 
-       return true;
-   }
-   return false;
+}
+
+function checkParamAsArray(arr) {
+    // if(arr.length>=0 || isArray(arr)){
+    if (arr.constructor === Array) {
+
+        return true;
+    }
+    return false;
+}
+
+function getSize(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        count++;
+    }
+    return count;
+}
+
+
+// этот метод извлекает первый элемент массива и возвращает его.
+//     При этом возвращенный элемент удаляется из массива, а длина массива уменьшается на единицу.
+//     Если массив пустой, то метод pop() возвращает значение undefined, при этом массив так и остается пустым
+
+function doPop(arr, task) {
+
+    if (arr.funcGetSize == 0) {
+        return undefined;
+    }
+    const firstDead = arr[0];
+    arrayTemp = [arr.funcGetSize - 1];
+    for (let i = 1; i < arr.funcGetSize; i++) {
+        arrayTemp[i - 1] = arr[i];
+        delete arr[i];
+    }
+    for (let i = 0; i < arrayTemp.length; i++) {
+        arr[i] = arrayTemp[i];
+        delete arrayTemp[i];
+    }
+    if(task = "return popedArray"){
+        return arr;
+    }
+    return firstDead;
+}
+
+
+// Метод добавляет элементы, начиная с текущей длины length и возвращает новую, увеличенную длину массива.
+function doPush(arr, newElement) {
+    arr[arr.length] = newElement;
+    if (arr[arr.length - 1] == "return pushed Array") {
+        return arr;
+    }
+    return arr.length;
+
 }
--- test.js
+++ test.js
@@ -2,15 +2,21 @@ const obj1 = {
     num :2,
     name : "test",
 };
+
 const objWithObject = {
     num : 2,
     name : "withObject",
     child : obj1,
 }
+
+const testInt = 5;
+const  testWord = "test";
 const array = [1,2,3,4,5];
 const array2 = [-1,-4,-1000000, 0];
 const arrayWithObject = [obj1, objWithObject];
 const arrayEmpty = [];
+const arrayPushedTestword = [1,2,3,4,5,"return pushed Array"];
+const popedArray = [2,3,4,5];
 
 describe("tests is this array", () => {
     const td = [
@@ -25,11 +31,12 @@ describe("tests is this array", () => {
             exp : true,
         },
         {
+            name : "array with objects ",
             a : arrayWithObject,
             exp : true,
         },
         {
-            name : "array with objects ",
+            name : "empty array ",
             a : arrayEmpty,
             exp : true,
         },
@@ -44,10 +51,10 @@ describe("tests is this array", () => {
             exp : false,
         }
     ]
-    for(let i=0; i<=td.length; i++) {
-    it(`test is this array  `+td[i].name, function () {
+    for(let i=0; i<td.length; i++) {
+    it('test is this array  '+td[i].name, function () {
 
-            const act = cP(td[i].a);
+            const act = checkParamAsArray(td[i].a);
             const expected = td[i].exp;
             assert.equal(act, expected);
     });
@@ -56,3 +63,85 @@ describe("tests is this array", () => {
 
  });
 
+describe("tests getSize of array", () => {
+    const td2 = [
+        {
+            name : "just array",
+            a : array,
+            exp : 5,
+        },
+        {
+            name : "array with (-) & 0 ",
+            a : array2,
+            exp : 4,
+        },
+        {
+            name : "array with objects ",
+            a : arrayWithObject,
+            exp : 2,
+        },
+        {
+            name : "empty array ",
+            a : arrayEmpty,
+            exp : 0,
+        }
+
+    ]
+    for(let i=0; i<td2.length; i++) {
+        it(`test getSize  `+td2[i].name, function () {
+
+            const act = getSize(td2[i].a);
+            const expected = td2[i].exp;
+            assert.equal(act, expected);
+        });
+    }
+
+
+});
+
+describe("tests of doPush ", () => {
+    const td = [
+        {
+            name : "add num to just array ",
+            a : array,
+            b : testInt,
+            exp : 6,
+        },
+        {
+            name : "add num to array with (-) & 0 ",
+            a : array2,
+            b :testWord,
+            exp : 5,
+        },
+        {
+            name : "add num to array with objects ",
+            a : arrayWithObject,
+            b : testInt,
+            exp : 3,
+        },
+        {
+            name : "add object to empty array ",
+            a : arrayEmpty,
+            b : obj1,
+            exp : 1,
+        },
+
+    ]
+    for(let i=0; i<td.length; i++) {
+        it('tests of doPush   '+td[i].name, function () {
+
+            const act = doPush(td[i].a);
+            const expected = td[i].exp;
+            assert.equal(act, expected);
+        });
+    }
+
+        it(`test add String to array. Check all elements `, function () {
+            for(let i = 0; i<arrayWithObject.length; i++) {
+                const act = doPush(array, "return pushed Array")[i];
+                const exp = arrayPushedTestword[i];
+                assert.equal(act, exp);
+            }
+        });
+
+});
